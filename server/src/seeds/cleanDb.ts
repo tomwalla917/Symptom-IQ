3272402cee744b5cc999849c2a4060ce603a3f7a
tomwalla917 <tywalla@gmail.com>
2026-04-02 19:08:23 -0400

    some minor changes, now the server and client side will run

--- server/src/seeds/cleanDb.ts
+++ server/src/seeds/cleanDb.ts
@@ -3,7 +3,10 @@ import db from '../config/connection.js';
 
 export default async (modelName: "Question", collectionName: string) => {
   try {
-    let modelExists = await models[modelName].db.db.listCollections({
+    const model = models[modelName];
+    if (!model?.db?.db) return;
+    
+    let modelExists = await model.db.db.listCollections({
       name: collectionName
     }).toArray()
 
