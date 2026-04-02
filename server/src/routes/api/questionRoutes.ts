46a8b99a24ee5c4f2766529bde630450d76e012f
Pamela Hornedo <pamhornedo@gmail.com>
2026-04-02 19:28:06 -0400

    deleted routes folder

--- server/src/routes/api/questionRoutes.ts
+++
@@ -1,7 +0,0 @@
-import express from "express";
-const router = express.Router();
-import { getRandomQuestions } from "../../controllers/symptomController.js";
-
-router.route("/random").get(getRandomQuestions);
-
-export default router;
