2460f0007d791271ade571f8c3a2f24dc3fa8026
tomwalla917 <tywalla@gmail.com>
2026-04-04 11:21:59 -0400

    i dont know what i did, but now were on the same page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,8 +1,10 @@
-import ReactDOM from 'react-dom/client'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './styles.css';
 
-import App from './App.js'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <App />
-)
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
