55c2e19649ba5742b9385e1ca0f99a82666fa555
Pamela Hornedo <pamhornedo@gmail.com>
2026-04-04 14:27:31 -0400

    adding run dev script on client side

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,5 @@
-import Quiz from './components/Quiz';
-
 function App() {
-  return (
-    <div className="App">
-      <Quiz />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
