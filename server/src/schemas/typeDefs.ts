422eff730b29c25f8ecdc35c0ae11766b66252d2
Pamela Hornedo <pamhornedo@gmail.com>
2026-04-03 21:44:34 -0400

    added missing update symptom mutation in typdefs

--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -37,6 +37,15 @@ const typeDefs = gql`
       createdAt: String
     ): Symptom
 
+    updateSymptom(
+      symptomType: String
+      severity: Int
+      duration: String
+      possibleTrigger: String
+      notes: String
+      createdAt: String
+    ): Symptom
+
     deleteSymptom(id: ID!): Symptom
 
     register(username: String!, email: String!, password: String!): AuthPayload!
