99a8d8dd4f2e23d0cedc5f0fc2aa1ad6b59c376d
Pamela Hornedo <pamhornedo@gmail.com>
2026-04-05 21:57:04 -0400

    adjusting mutation names to match code and imports

--- client/src/graphql/queries.ts
+++ client/src/graphql/queries.ts
@@ -1,112 +1,138 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const ME_QUERY = gql`
-    query Me {
-        me {
-            _id
-            username
-            email
-        }
+  query Me {
+    me {
+      _id
+      username
+      email
     }
+  }
 `;
 
 export const GET_SYMPTOMS = gql`
-    query getSymptoms {
-        getSymptoms {
-            _id
-            symptomType
-            severity
-            createdAt
-        }
+  query getSymptoms {
+    getSymptoms {
+      _id
+      symptomType
+      severity
+      createdAt
     }
+  }
 `;
 
 export const GET_SYMPTOM = gql`
-    query getSymptom($id: ID!) {
-        getSymptom(id: $id) {
-            _id
-            userId
-            symptomType
-            severity
-            duration
-            possibleTrigger
-            notes
-            createdAt
-        }
+  query getSymptom($id: ID!) {
+    getSymptom(id: $id) {
+      _id
+      userId
+      symptomType
+      severity
+      duration
+      possibleTrigger
+      notes
+      createdAt
     }
+  }
 `;
 
 export const REGISTER_MUTATION = gql`
-    mutation Register($username: String!, $email: String!, $password: String!) {
-        register(username: $username, email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-                email
-            }
-        }
+  mutation Register($username: String!, $email: String!, $password: String!) {
+    register(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+        email
+      }
     }
+  }
 `;
 
 export const LOGIN_MUTATION = gql`
-    mutation Login($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-            token
-            user {
-                _id
-                username
-                email
-            }
-        }
-    }
-`;
-
-export const DELETE_SYMPTOM_MUTATION = gql`
-    mutation deleteSymptom($id: ID!) {
-        deleteSymptom(id:$id){
+  mutation Login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
         _id
-        userId
-        symptomType
-        severity
-        duration
-        possibleTrigger
-        notes
-        createdAt
-        }
+        username
+        email
+      }
     }
+  }
 `;
 
-export const ADD_SYMPTOM_MUTATION = gql`
-    mutation addSymptom($userId: ID!, $symptomType: String!, $severity: Int!,
-    $duration: String!, $possibleTrigger: String, $notes: String) {
-        addSymptom(userId: $userId, symptomType: $symptomType, severity: $severity,
-        duration: $duration, possibleTrigger: $possibleTrigger, notes: $notes) {
-            _id
-            userId
-            symptomType
-            severity
-            duration
-            possibleTrigger
-            notes
-            createdAt
-        }
+export const DELETE_SYMPTOM = gql`
+  mutation deleteSymptom($id: ID!) {
+    deleteSymptom(id: $id) {
+      _id
+      userId
+      symptomType
+      severity
+      duration
+      possibleTrigger
+      notes
+      createdAt
     }
+  }
 `;
 
-export const UPDATE_SYMPTOM_MUTATION = gql`
-    mutation updateSymptom($id: ID!, $userId: ID!, $symptomType: String!, $severity: Int!,
-    $duration: String!, $possibleTrigger: String, $notes: String) {
-        updateSymptom(id: $id, userId: $userId, symptomType: $symptomType, severity: $severity,
-        duration: $duration, possibleTrigger: $possibleTrigger, notes: $notes) {
-            _id
-            userId
-            symptomType
-            severity
-            duration
-            possibleTrigger
-            notes
-            createdAt
-        }
+export const ADD_SYMPTOM = gql`
+  mutation addSymptom(
+    $userId: ID!
+    $symptomType: String!
+    $severity: Int!
+    $duration: String!
+    $possibleTrigger: String
+    $notes: String
+  ) {
+    addSymptom(
+      userId: $userId
+      symptomType: $symptomType
+      severity: $severity
+      duration: $duration
+      possibleTrigger: $possibleTrigger
+      notes: $notes
+    ) {
+      _id
+      userId
+      symptomType
+      severity
+      duration
+      possibleTrigger
+      notes
+      createdAt
     }
+  }
+`;
+
+export const UPDATE_SYMPTOM = gql`
+  mutation updateSymptom(
+    $id: ID!
+    $userId: ID!
+    $symptomType: String!
+    $severity: Int!
+    $duration: String!
+    $possibleTrigger: String
+    $notes: String
+  ) {
+    updateSymptom(
+      id: $id
+      userId: $userId
+      symptomType: $symptomType
+      severity: $severity
+      duration: $duration
+      possibleTrigger: $possibleTrigger
+      notes: $notes
+    ) {
+      _id
+      userId
+      symptomType
+      severity
+      duration
+      possibleTrigger
+      notes
+      createdAt
+    }
+  }
 `;
