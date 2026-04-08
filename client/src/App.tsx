b592b2253f4032e223fd9922b29caa12a0ddf0ac
Pamela Hornedo <pamhornedo@gmail.com>
2026-04-07 20:19:06 -0400

    adding protected route for pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,15 +3,37 @@ import UserAuth from "./pages/userAuth";
 import Dashboard from "./pages/dashboard";
 import AddSymptom from "./pages/addSymptom";
 import EditSymptom from "./pages/editSymptom";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<UserAuth />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/add-symptom" element={<AddSymptom />} />
-        <Route path="/edit-symptom" element={<EditSymptom />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/add-symptom"
+          element={
+            <ProtectedRoute>
+              <AddSymptom />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/edit-symptom"
+          element={
+            <ProtectedRoute>
+              <EditSymptom />
+            </ProtectedRoute>
+          }
+        />
       </Routes>
     </div>
   );
