5a7b6e7e954033b59adb9945f7d3d54256c0544d
Pamela Hornedo <pamhornedo@gmail.com>
2026-04-09 19:53:37 -0400

    creating reusable card for stats

--- client/src/components/StatsCard.tsx
+++ client/src/components/StatsCard.tsx
@@ -1,8 +1,22 @@
-const StatsCard = () => {
+import React from "react";
+
+interface CardProps {
+  title: string;
+  value: string | number;
+  icon: React.ReactNode;
+}
+
+const StatsCard: React.FC<CardProps> = ({ title, value, icon }) => {
   return (
-    <div>
-      <p>Stats card</p>
+    <div className="stats">
+      <div className="stats-header">
+        <span>{title}</span>
+        <span>{icon}</span>
+      </div>
+
+      <div className="stats-value">{value}</div>
     </div>
   );
 };
+
 export default StatsCard;
